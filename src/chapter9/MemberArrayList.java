f2c2bb8b7b138885f158f1d8d2365398a133cec0
seungo <ilkhso@gmail.com>
2023-07-20 08:47:19 +0900

    feat: collection framework - iterator

--- src/chapter9/MemberArrayList.java
+++ src/chapter9/MemberArrayList.java
@@ -1,6 +1,7 @@
 package chapter9;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class MemberArrayList {
     private final ArrayList<Member> arrayList;
@@ -14,11 +15,14 @@ public class MemberArrayList {
     }
 
     public boolean removeMember(int memberId) {
-        for (int i = 0; i < arrayList.size(); i++) {
-            Member member = arrayList.get(i);
 
+
+        Iterator<Member> ir = arrayList.iterator();
+
+        while (ir.hasNext()) {
+            Member member = ir.next();
             if (member.getMemberId() == memberId) {
-                arrayList.remove(i);
+                arrayList.remove(member);
                 return true;
             }
         }
