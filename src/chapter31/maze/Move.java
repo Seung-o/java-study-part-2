4744f1e4bad0d7e218624ffe50f614f1b5165194
seungo <ilkhso@gmail.com>
2023-08-04 09:20:49 +0900

    feat: maze problem

---
+++ src/chapter31/maze/Move.java
@@ -0,0 +1,15 @@
+package chapter31.maze;
+
+public class Move {
+
+    public int x = 0;
+    public int y = 0;
+    int direction = 0;
+
+    public Move(int x, int y) {
+        this.x = x;
+        this.y = y;
+
+    }
+
+}
